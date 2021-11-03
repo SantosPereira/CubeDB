96780ccd8c3e5d89c45508764ed3ddc8c70d69cc
SantosPereira <pedrohenriquelemam@gmail.com>
2021-11-03 11:49:11 -0300

    Implementação da classe principal e alguns métodos

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CubicPoolDB
-Implementação de banco de dados em memória
+
+![](md-media/CubicPoolDB.png)
+
+Implementação de banco de dados em memória de alta performace
+
+## Instalação
+
+Para usufluir de uma melhoria de performace consideravél, se comparado ao código-fonte,
+é necessário compila-lo através do comando:
+
+~~~bash
+$ python -m py_compile main.py
+~~~
---
+++ main.py
@@ -0,0 +1,28 @@
+class CubicPool:
+    def __init__(self) -> None:
+        self.__row: dict = {}
+        self.__pool: list = []
+        self.cube: dict = {}
+
+
+    def criarTabela(self, nomeTabela, tabela=[]) -> None:
+        self.cube.update({nomeTabela:tabela})
+            
+
+    def inserirDados(self, tabela, dados) -> None:
+        self.cube[tabela].append(dados)
+
+
+
+CubicPool = CubicPool()
+CubicPool.criarTabela('Tabela2')
+CubicPool.inserirDados(
+    'Tabela2',
+    {
+        'id': 2,
+        'Nome': 'Pedro',
+        'Idade': 19
+    }
+)
+
+print(CubicPool.cube)
